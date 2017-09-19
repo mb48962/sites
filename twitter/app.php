e6606cf4e9586396261fb99beec8447ac7822e6a
mb48962 <mb48962@vt.edu>
2017-09-18 22:59:33 -0400

    Twitter folder

---
+++ twitter/app.php
@@ -0,0 +1,14 @@
+<?php
+require_once('TwitterAPIExchange.php');
+
+$settings = array(
+    'oauth_access_token' => "1517890854-M3KSvLUrlT4pYKc5ftehmTuCS9ukRexYnIbg9rT",
+    'oauth_access_token_secret' => "cYMY5Pehr2mKXdpmPn3K7J3xUI4Ifnfsssctdch8nsQv2",
+    'consumer_key' => "8btgkkENoxJGVPFhuhzWg3QDz",
+    'consumer_secret' => "ay88Z9Wqf7QFL6vDm1CLa7LpAt55EipE3ndoFlzslHuN25pfKW"
+);
+
+$url = 'https://api.twitter.com/1.1/blocks/create.json';
+$requestMethod = 'POST';
+
+?>
